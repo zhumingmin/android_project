762f85f58df55792a17ff1777f5facf6c3182d36
My Android Project <zhumingmin@outlook.com>
2016-07-07 16:08:39 +0800

    民兴村手机端部分代码修改

--- MinXing-android/src/com/minxing/restwebservice/BaoJianService.java
+++ MinXing-android/src/com/minxing/restwebservice/BaoJianService.java
@@ -5,7 +5,9 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.List;
 
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
@@ -18,6 +20,8 @@ import org.apache.http.params.BasicHttpParams;
 import org.apache.http.params.HttpConnectionParams;
 import org.apache.http.params.HttpParams;
 import org.apache.http.protocol.HTTP;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import com.minxing.util.CustomArrayAdapter;
@@ -29,8 +33,13 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.res.AssetManager;
 import android.graphics.Typeface;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
@@ -64,6 +73,9 @@ public class BaoJianService extends Activity {
 	int m_count = 0;
 	LinearLayout ly_baojian;
 	private CustomArrayAdapter<CharSequence> mAdapter;
+	public static final int SHOW_LOCATION = 0;
+	private LocationManager locationManager;
+	private String provider;
 
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -158,7 +170,123 @@ public class BaoJianService extends Activity {
 				DisplayToast("网上查询，暂未开通");
 			}
 		});
+
+		/*
+		 * 通过locationManager实现定位功能（将经纬度通过Geocoding API进行反向解码）
+		 */
+		locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+		// 获取所有可用的位置提供器
+		List<String> providerList = locationManager.getProviders(true);
+		if (providerList.contains(LocationManager.NETWORK_PROVIDER)) {
+			provider = LocationManager.GPS_PROVIDER;
+		} else if (providerList.contains(LocationManager.GPS_PROVIDER)) {
+			provider = LocationManager.NETWORK_PROVIDER;
+		} else {
+			// 当没有可用的位置提供器时，弹出Toast提示用户
+			Toast.makeText(this, "No location provider to use",
+					Toast.LENGTH_SHORT).show();
+			return;
+		}
+		Location location = locationManager.getLastKnownLocation(provider);
+		if (location != null) {
+			// 显示当前设备的位置信息
+			showLocation(location);
+		}
+		locationManager.requestLocationUpdates(provider, 5000, 1,
+				locationListener);
 	}
+
+	protected void onDestroy() {
+		super.onDestroy();
+		if (locationManager != null) {
+			// 关闭程序时将监听器移除
+			locationManager.removeUpdates(locationListener);
+		}
+	}
+
+	LocationListener locationListener = new LocationListener() {
+
+		@Override
+		public void onStatusChanged(String provider, int status, Bundle extras) {
+		}
+
+		@Override
+		public void onProviderEnabled(String provider) {
+		}
+
+		@Override
+		public void onProviderDisabled(String provider) {
+		}
+
+		@Override
+		public void onLocationChanged(Location location) {
+			// 更新当前设备的位置信息
+			showLocation(location);
+		}
+	};
+
+	private void showLocation(final Location location) {
+		// String currentPosition =
+		// "latitude is"+location.getLatitude()+"\n"+"longitude is"+location.getLongitude();
+		// et_baojiandizhi.setText(currentPosition);
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					// 组装反向地理编码的接口地址
+					StringBuilder url = new StringBuilder();
+					url.append("http://maps.googleapis.com/maps/api/geocode/json?latlng=");
+					url.append(location.getLatitude()).append(",")
+							.append(location.getLongitude());
+					url.append("&sensor=true");
+					// AIzaSyB5LUMOXNvNn0DiLnHGJY9wSfqj-e4wJkE
+					HttpClient httpClient = new DefaultHttpClient();
+					HttpGet httpGet = new HttpGet(url.toString());
+					// 在请求消息头中指定语言，保证服务器会返回中文数据
+					httpGet.addHeader("Accept-Language", "zh-CN");
+					HttpResponse httpResponse = httpClient.execute(httpGet);
+					if (httpResponse.getStatusLine().getStatusCode() == 200) {
+						HttpEntity entity = httpResponse.getEntity();
+						String response = EntityUtils.toString(entity, "utf-8");
+						JSONObject jsonObject = new JSONObject(response);
+						// 获取results节点下的位置信息
+						JSONArray resultArray = jsonObject
+								.getJSONArray("results");
+						if (resultArray.length() > 0) {
+							JSONObject subObject = resultArray.getJSONObject(0);
+							// 取出格式化后的位置信息
+							String address = subObject
+									.getString("formatted_address");
+							Message message = new Message();
+							message.what = SHOW_LOCATION;
+							message.obj = address;
+							handler.sendMessage(message);
+						}
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}).start();
+	}
+
+	/*
+	 * 开启一个线程来实现位置的显示
+	 */
+	private Handler handler = new Handler() {
+
+		public void handleMessage(Message msg) {
+			switch (msg.what) {
+			case SHOW_LOCATION:
+				String currentPosition = (String) msg.obj;
+				et_baojiandizhi.setText(currentPosition);
+				break;
+			default:
+				break;
+			}
+		}
+
+	};
 
 	public void postData(View vw) {
 		TextView tx_spinner1 = (TextView) sp_baojianxiangmu.getSelectedView();
@@ -168,9 +296,12 @@ public class BaoJianService extends Activity {
 		TextView tx_spinner2 = (TextView) sp_yuyueshijian.getSelectedView();
 		String yuyueshijian = (String) tx_spinner2.getText();
 
-		if (baojianxiangmu.equals("") || baojiandizhi.equals("")
-				|| lianxifangshi.equals("") || yuyueshijian.equals("")) {
-			Toast.makeText(this, "您还有项目未填写哦！", Toast.LENGTH_LONG).show();
+		if (baojiandizhi.equals("")) {
+			Toast.makeText(getApplicationContext(), "报建地址不能为空！", 0).show();
+			return;
+		}
+		if (lianxifangshi.equals("")) {
+			Toast.makeText(getApplicationContext(), "联系方式不能为空！", 0).show();
 			return;
 		}
 
