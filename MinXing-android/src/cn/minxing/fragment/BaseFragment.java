9cdc5874509c26c7f5fefdf06a255a402167bbda
My Android Project <zhumingmin@outlook.com>
2016-12-09 14:35:25 +0800

    12.9重要更新
    修复已知问题。

---
+++ MinXing-android/src/cn/minxing/fragment/BaseFragment.java
@@ -0,0 +1,41 @@
+package cn.minxing.fragment;
+
+import android.support.v4.app.Fragment;
+
+public abstract class BaseFragment extends Fragment {
+
+	/** Fragment当前状态是否可见 */
+	protected boolean isVisible;
+
+	@Override
+	public void setUserVisibleHint(boolean isVisibleToUser) {
+		super.setUserVisibleHint(isVisibleToUser);
+
+		if (getUserVisibleHint()) {
+			isVisible = true;
+			onVisible();
+		} else {
+			isVisible = false;
+			onInvisible();
+		}
+	}
+
+	/**
+	 * 可见
+	 */
+	protected void onVisible() {
+		lazyLoad();
+	}
+
+	/**
+	 * 不可见
+	 */
+	protected void onInvisible() {
+
+	}
+
+	/**
+	 * 延迟加载 子类必须重写此方法
+	 */
+	protected abstract void lazyLoad();
+}
